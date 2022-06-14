8cc9fcf9bf73ee9baa466fbbacfbbe5aaa435d1c
Andy Stringer <andy.stringer@gmail.com>
2022-06-14 17:24:49 +0100

    Refressh all

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,9 +4,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -16,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'gco-timer'`, () => {
+  it(`should have as title 'nif-simple'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('gco-timer');
+    expect(app.title).toEqual('nif-simple');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('gco-timer app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'nif-simple app is running!'
+    );
   });
 });
